d36fbebe1a2c3f3b4e71a863e7c251de71d1ada8
MateoMendez07 <henry.mendez549@ist17dejulio.edu.ec>
2024-12-18 07:43:15 -0500

    Figuras geometricas

--- index.js
+++ index.js
@@ -30,7 +30,32 @@ app.get('/sumas/:n1',(req,resp)=>{
     let num1 = parseInt(req.params.n1);
     let num2 = 3;
     let suma = num1 + num2;
-    resp.send('el resultado es: '+ suma);
+    resp.send('El resultado es: '+ suma);
+});
+
+app.get('/cuadrado/areayperimetro/:n1',(req,resp)=>{
+    let lado = parseInt(req.params.n1);
+    let area = lado * lado;
+    let perimetro = lado * 4;
+    resp.send('El area del cuadrado es: ' + area + '\nEl perimetro es: ' + perimetro);
+});
+
+app.get('/circulo/areayperimetro/:n1',(req,resp)=>{
+    let pi = Math.PI;
+    let radio = parseInt(req.params.n1);
+    let area = (radio * radio) * pi;
+    let perimetro = radio * (pi*2);
+    resp.send('El area del ciruclo es: ' + area + '\nEl perimetro es: ' + perimetro);
+});
+
+app.get('/paralelogramo/areayperimetro/:n1/:n2/:n3',(req,resp)=>{
+        let base = parseInt(req.params.n1);
+        let altura = parseInt(req.params.n2);
+        let lado = parseInt(req.params.n3);
+        let area = base * altura;
+        let perimetro = 2 * (base + lado);
+
+        resp.send('El area del paralelogramo es: ' + area + '\nEl perimetro es: ' + perimetro)
 });
 app.listen(3000,()=>{
     console.log('Esta funcionando');
